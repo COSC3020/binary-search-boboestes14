c9949e0d4d84addf613cfec00094999de7b0e086
boboestes14 <143558849+boboestes14@users.noreply.github.com>
2023-09-20 01:42:26 +0000

    commit

--- code.js
+++ code.js
@@ -1,14 +1,15 @@
 function binarySearch(list, element) {
     i = 0;
     while(i<list.length){
-        if(list[i] = element)
+        if(list[i] == element)
             return i;
 
-        if(list[i+2] <= element)
-            i = i*2+2;
-        else
-            i = i*2+1;
-
+        if(list[i+2] <= element){
+            i = ((i*2)+2);
+        }
+        else{
+            i = (i*2)+1;
+        }
     }
     return -1;
 }
